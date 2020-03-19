fbba57120bafbab90a6582d61a55127c81d2595e
Sergio Moreno <sergio.m.calzada@gmail.com>
2020-03-19 15:35:07 +0100

    add dbcontext

---
+++ src/Sereno.Domain.EntityFramework/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Sereno.Domain.Entity;
+
+namespace Sereno.Domain.EntityFramework
+{
+    public class DataContext : IdentityDbContext<User, Role, string, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
