013f87e10f0c3cadf61a3b0f41063d7353c1973f
Sergio Moreno <sergio.m.calzada@gmail.com>
2020-03-20 03:38:46 +0100

    configure services and middlewares

---
+++ src/Sereno.STS/DependencyInjection/AspNetIdentityExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Sereno.Domain.Entity;
+using Sereno.STS.AspNetIdentity;
+using Sereno.STS.DependencyInjection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class AspNetIdentityExtensions
+    {
+        public static IServiceCollection AddAspNetIdentity(this IServiceCollection services)
+        {
+
+            services.AddIdentity<User, Role>()
+                .AddUserStore<UserStore>()
+                .AddUserManager<UserManager>()
+                .AddRoleStore<RoleStore>()
+                .AddRoleManager<RoleManager>()
+                .AddSignInManager<SignInManager>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IUserClaimsPrincipalFactory<User>, UserClaimsPrincipalFactory>();
+            services.AddScoped<ISecurityStampValidator, Sereno.STS.AspNetIdentity.SecurityStampValidator>();
+            services.AddSingleton<IPostConfigureOptions<IdentityOptions>, PostConfigureIdentityOptions>();
+
+            return services;
+        }
+    }
+
+
+}
---
+++ src/Sereno.STS/DependencyInjection/PostConfigureIdentityOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace Sereno.STS.DependencyInjection
+{
+    public class PostConfigureIdentityOptions : IPostConfigureOptions<IdentityOptions>
+    {
+        public void PostConfigure(string name, IdentityOptions options)
+        {
+            options.User.RequireUniqueEmail = true;
+            options.User.AllowedUserNameCharacters = options.User.AllowedUserNameCharacters + "\\" + "#";
+        }
+    }
+}
