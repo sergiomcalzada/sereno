8e13fa581aec790160ee7483a446b870e5e522cb
Sergio Moreno <sergio.m.calzada@gmail.com>
2020-09-01 09:20:51 +0200

    remove dependencies on Consent Controller

--- src/Sereno.STS.UI/Pages/Consent/Index.cshtml.cs
+++ src/Sereno.STS.UI/Pages/Consent/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IdentityServer4;
@@ -11,8 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Sereno.STS.UI.Controllers;
-using Sereno.STS.UI.Controllers.Consent;
-using static System.String;
+
 
 namespace Sereno.STS.UI.Pages.Consent
 {
@@ -74,7 +74,7 @@ namespace Sereno.STS.UI.Pages.Consent
 
             if (result.HasValidationError)
             {
-                this.ModelState.AddModelError(Empty, result.ValidationError);
+                this.ModelState.AddModelError(string.Empty, result.ValidationError);
             }
 
             if (result.ShowView)
@@ -236,7 +236,7 @@ namespace Sereno.STS.UI.Pages.Consent
         public ScopeModel CreateScopeViewModel(ParsedScopeValue parsedScopeValue, ApiScope apiScope, bool check)
         {
             var displayName = apiScope.DisplayName ?? apiScope.Name;
-            if (!IsNullOrWhiteSpace(parsedScopeValue.ParsedParameter))
+            if (!string.IsNullOrWhiteSpace(parsedScopeValue.ParsedParameter))
             {
                 displayName += ":" + parsedScopeValue.ParsedParameter;
             }
@@ -293,6 +293,16 @@ namespace Sereno.STS.UI.Pages.Consent
 
             public bool HasValidationError => this.ValidationError != null;
             public string ValidationError { get; set; }
+        }
+
+        public class ConsentOptions
+        {
+            public static bool EnableOfflineAccess = true;
+            public static string OfflineAccessDisplayName = "Offline Access";
+            public static string OfflineAccessDescription = "Access to your applications and resources, even when you are offline";
+
+            public static readonly string MustChooseOneErrorMessage = "You must pick at least one permission";
+            public static readonly string InvalidSelectionErrorMessage = "Invalid selection";
         }
     }
 }
