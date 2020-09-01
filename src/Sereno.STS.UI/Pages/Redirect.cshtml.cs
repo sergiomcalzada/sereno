3a7972bc50a79d45118ca6d1273668c9769b9f75
Sergio Moreno <sergio.m.calzada@gmail.com>
2020-09-01 10:58:37 +0200

    Added redirect page

---
+++ src/Sereno.STS.UI/Pages/Redirect.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sereno.STS.UI.Pages
+{
+    public class RedirectModel : PageModel
+    {
+        public string RedirectUrl { get; set; }
+
+        public IActionResult OnGet(string redirectUrl)
+        {
+            this.RedirectUrl = redirectUrl;
+            return this.Page();
+        }
+    }
+}
